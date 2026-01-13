5a555e81cab39f79625579e121b683addef3f994
xavier2929 <castilloxavier49@gmail.com>
2026-01-12 19:49:54 -0700

    segundo commit por esta clase, se usaron los metodos puestos

--- Clase_1-12-2025/ReadMe.md
+++ Clase_1-12-2025/ReadMe.md
@@ -7,3 +7,35 @@ para los proyectos complejos.
 
 Aplique algo interesante que desde la consola si usamos
 python nombredeScript.py se corre y tarda menos en debuggear
+
+el metodo .index() sirve para traer cierta cadena de caracteres, de izquierda a derecha, este nos trae la posicion
+de donde se encuentra un caracter o substring
+
+el metodo upper() nos sirve para pasar a mayusculas todos los
+caracteres de una cadena.
+
+el metodo lower() es lo inverso que vimos en el uppder(), convierte a todos los carateres a minisculas
+
+El metodo split() corta un string a una lista de string, usando un char como separados
+
+El metodo join() es lo contrario, podemos unir varios string
+y crear uno solo importante de usar un char o string " " o
+' 'como el que va a unir los string " ".join (lista) o
+" ".join(["Hi", "Mom"])
+
+El metodo find() nos encuentra en que posicion se encuentra un substring y devuelve la primera coincidencia.
+
+El metodo replace() sustituye substring, el primer str es el ubstr a buscar y el segundo por el que vamos a sustituir
+
+El metodo isdecimal() nos regresa si un str es un numero o no
+
+El metodo isalnum() regresa si todos los caractres son letras o numeros
+
+El metodo isalpha() detecta las letras del alfabet y nos devuelve una respuesta booleana
+
+El metodo isspace() nos regresa una respuesta booleana si el str contiene puros espacios
+
+El metodo capitalize() pone en mayuscula el primer char
+de un str.
+
+El metodo swapcase() invierte cada char, si estaba en miniscula ahora estara en mayuscula, si estaba en mayuscula ahora estara en miniscula.
--- Clase_1-12-2025/script.py
+++ Clase_1-12-2025/script.py
@@ -9,3 +9,70 @@ print(fragmento2)
 print(fragmento3)
 fragment4 = texto[2:5]
 print(fragment4)
+
+
+print("corte en secuencia")
+
+corte_de_dos_en_dos = texto[: : 2] # crea una cadena agarrando el char cada 2 caracteres
+print(corte_de_dos_en_dos)
+
+
+corte_de_dos_en_dos = texto[: : -2] # de dos en dos, pero de reversa
+print(corte_de_dos_en_dos)
+
+print("Funcion de .index() ")
+
+texto = "esta es una prueba"
+text_con_index = texto.index("a")
+print(text_con_index)
+
+numeros = (1,2,3,2,5,2)
+numbers_w_index = numeros.index(3,0,5)
+
+print(numbers_w_index)
+
+print("metodo upper")
+texto= "Hoy vamos a ver seis metodos"
+uppder_result = texto.upper()
+print(f'este es la variable normal \n "{texto}" y a uppder es \n {uppder_result}')
+
+print("metodo lower")
+lower_result = texto.lower()
+print(f'este es la variable normal \n "{texto}" y a lower es \n "{lower_result}"')
+
+
+print("ahora veremos el metodo split")
+split_result = texto.split(' ')
+print(split_result)
+print(texto.split('o') )
+
+print("ahora vamos a usar join()")
+
+list_to_reverse_to_str = ['Hoy', 'vamos', 'a', 'ver', 'seis', 'metodos']
+joined_list = " ".join(list_to_reverse_to_str)
+print(joined_list)
+
+print("metodo find")
+print(joined_list.find("metodo"))
+
+print("metodo replace()")
+
+print(joined_list.replace("metodo","calacas"))
+
+print("metodo isdecimal()")
+print("2.55".isdecimal())
+
+print("metodo isalnum")
+print("123ABC".isalnum())
+
+print("metodo isalnum()")
+print("123ABC".isalpha())
+
+print("metodo isspace()")
+print("  ".isspace())
+
+print("metodo capitalize()")
+print("hi .mom!".capitalize())
+
+print("metmdo swapcase() ")
+print("Hi Everyone!".swapcase())
